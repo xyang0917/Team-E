d8fbb1c0e2fec95ca52578648ca2ed6385a1bb24
yangxin <xyang0917@gmail.com>
2018-03-14 12:12:32 +0800

    修改员工前，先结清之前员工的工资

--- Lesson-1/assignment/Payroll.sol
+++ Lesson-1/assignment/Payroll.sol
@@ -50,7 +50,12 @@ contract Payroll {
 
     function changeEmployee(address emp) {
         require(msg.sender == owner);
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
         employee = emp;
+        lastPayday = now;
     }
     
     function changeSalary(uint s) {
