2df95fb6620b990986e6db6623123621dd3f82c9
yangxin <xyang0917@gmail.com>
2018-04-02 23:01:31 +0800

    部署到测试网络

--- Lesson-6/assignment/payroll/truffle.js
+++ Lesson-6/assignment/payroll/truffle.js
@@ -1,9 +1,28 @@
+
+var HDWalletProvider = require("truffle-hdwallet-provider") // 调用以太坊钱包的中间库
+const infura_apikey = 'EH9RCfLTQttKVqg1OS1W'    // infura申请的apikey
+const mnemonic = 'early submit sleep rabbit viable hover divide prevent song water rocket crush'         // 钱包助记词
+
 module.exports = {
     networks: {
         development: {
             host: "127.0.0.1",
             port: 8545,
-            network_id: "*" // Match any network id
+            network_id: "*"
+        },
+        ropsten: {
+            provider: function () {
+                return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey)  // 最后一个参数指定助记词恢复的帐号索引
+            },
+            network_id: 3,
+        },
+        main: {
+            provider: function () {
+                return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/" + infura_apikey)
+            },
+            network_id: 3,
+            gas: 3012388,
+            gasPrice: 1000000000
         }
     }
 };
